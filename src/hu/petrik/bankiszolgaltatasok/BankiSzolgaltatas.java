9ea827f5b9be842544673fbcf37ec0b3bf4bc68f
JungKata <97729333+JungKata@users.noreply.github.com>
2022-10-23 11:24:48 +0200

    3.feladat tulajdonos construktor getter olvashatosag miatt gitignore

---
+++ src/hu/petrik/bankiszolgaltatasok/BankiSzolgaltatas.java
@@ -0,0 +1,15 @@
+package hu.petrik.bankiszolgaltatasok;
+
+public abstract class BankiSzolgaltatas {
+    private Tulajdonos tulajdonos;
+
+    public Tulajdonos getTulajdonos() {
+        return tulajdonos;
+    }
+
+    public BankiSzolgaltatas(Tulajdonos tulajdonos) {
+        this.tulajdonos = tulajdonos;
+
+
+    }
+}
