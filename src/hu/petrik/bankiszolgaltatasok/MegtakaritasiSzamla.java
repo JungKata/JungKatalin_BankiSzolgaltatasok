f7bb724221b422da24681d4c9c274052346b1485
JungKata <97729333+JungKata@users.noreply.github.com>
2022-10-23 17:05:02 +0200

    6. feladat megtakaritasiSzamla ostály létrehozása származtatása a szamla osztalybol double valtozo letrehozasa kamatmertek construktor, getter, setter kivesz fuggvény 0-nál nagyobbnál

--- src/hu/petrik/bankiszolgaltatasok/MegtakaritasiSzamla.java
+++ src/hu/petrik/bankiszolgaltatasok/MegtakaritasiSzamla.java
@@ -2,10 +2,11 @@ package hu.petrik.bankiszolgaltatasok;
 
 public class MegtakaritasiSzamla extends Szamla{
     private double kamatmerteke;
+    public static double alapKamatErtek = 1.1;
 
-    public MegtakaritasiSzamla(Tulajdonos tulajdonos, int aktEgyenleg, double kamatmerteke) {
+    public MegtakaritasiSzamla(Tulajdonos tulajdonos, int aktEgyenleg) {
         super(tulajdonos, aktEgyenleg);
-        this.kamatmerteke = kamatmerteke;
+        kamatmerteke = alapKamatErtek;
     }
 
     public double getKamatmerteke() {
@@ -14,5 +15,15 @@ public class MegtakaritasiSzamla extends Szamla{
 
     public void setKamatmerteke(double kamatmerteke) {
         this.kamatmerteke = kamatmerteke;
+    }
+
+    @Override
+    public boolean kiveszOsszeg(int osszeg) {
+        if (getAktEgyenleg() - osszeg >= 0) {
+            this.aktEgyenleg -= osszeg;
+            return true;
+        }else {
+        return false;
+        }
     }
 }
